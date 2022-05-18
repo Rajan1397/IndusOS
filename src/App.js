bc4100470870aebd52685584e07b9350fb407309
Rajan Kumar <rajnkumar.1397@gmail.com>
2022-05-18 14:30:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+import React, { useState } from "react";
+
+const App = () => {
+  const [input, setInput] = useState("");
+
+  const map = {
+    "::happy::": "😀",
+    "::sad::": "😢",
+    "::smiley::": "😁",
+  };
+
+  const handleChange = (e) => {
+    let { value } = e.target;
+
+    var replace = new RegExp(Object.keys(map).join("|"), "gi");
+    value = value.replace(replace, function (found) {
+      return map[found];
+    });
+
+    setInput(value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Text to Emoji Converter</h1>
+      <input
+        type="text"
+        onChange={handleChange}
+        value={input}
+        style={{ padding: "10px", width: "80%^" }}
+        placeholder="Type your text here to convert it into an emoji"
+      />
     </div>
   );
-}
+};
 
 export default App;
